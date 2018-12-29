2f446072dc672f96f2f3c91f1aecc96302854865
Adam <Adam.Whitehurst@live.com>
2018-12-29 15:25:35 -0800

    Add data messaging

--- public/sketch.js
+++ public/sketch.js
@@ -1,22 +1,15 @@
 /// <reference path="./p5/p5.global-mode.d.ts" />
 
+// DOM Elements
+let inputTextElement;
+let inputButtonElement;
+let canvasElement;
+
+// p5 elements
+let canvas;
 let currAnt;
-/*  ANT.DRAWING[] FORMAT:
- *   Each index in ant.drawing[] is a single stroke
- *   a stroke is a single line from pen down to pen up
- *   NOTE: 'stroke' is called 'path'
- *   because stroke() is a p5 function
- *
- *   Each path in ant.drawing[][] has two indexes:
- *   [0] the x values for each vertex of the line
- *   [1] the y values for each vertex of the line
- *
- *   Thus, a single vertex of a specific path is:
- *   {
- *     x: ant.drawing[path][0][i],
- *     y: ant.drawing[path][1][i]
- *   }
- */
+
+let ctx;
 
 // DRAWING INDEXES
 let pi = 0; // Path index
@@ -24,8 +17,27 @@ let li = 0; // Line (in a path) index
 let prevX;
 let prevY;
 
+// called by p5
 function setup() {
-  createCanvas(510, 510);
+  canvas = createCanvas(510, 510);
+  canvas.parent("sketch-holder");
+
+  inputTextElement = document.getElementById("textInput");
+
+  inputButtonElement = document.getElementById("button");
+  inputButtonElement.addEventListener("click", submitForm);
+
+  formElement = document.getElementById("form");
+  formElement.addEventListener("submit", submitForm);
+
+  canvasElement = document.getElementById("defaultCanvas0");
+  ctx = canvasElement.getContext("2d");
+
+  init();
+}
+
+function init() {
+  clear();
   background(24);
   noFill();
   stroke(255);
@@ -35,6 +47,7 @@ function setup() {
   retrieveAnt();
 }
 
+// called by p5
 function draw() {
   if (currAnt) {
     const x = currAnt[pi][0][li] + 127;
@@ -53,25 +66,60 @@ function draw() {
   }
 }
 
+function submitForm(data) {
+  data.preventDefault();
+  sendImageData(ctx.getImageData(0, 0, 510, 510).data);
+  init();
+}
+
 function checkBounds() {
   if (li >= currAnt[pi][0].length) {
-    pi++;
-    li = 0;
-    prevX = null;
-    prevY = null;
+    resetLine();
 
-    if (pi === currAnt.length) {
-      pi = 0;
-      clear();
-      currAnt = null;
-      setup();
+    if (pi >= currAnt.length) {
+      reset();
     }
   }
 }
-function gotAnt(ant) {
-  currAnt = ant.drawing;
+
+function resetLine() {
+  pi++;
+  li = 0;
+  prevX = null;
+  prevY = null;
+}
+
+function reset() {
+  pi = 0;
+  currAnt = null;
+}
+
+function sendImageData(data) {
+  socket.emit("send image data", data);
 }
 
 function retrieveAnt() {
   loadJSON("/getAnt", gotAnt);
+}
+
+function gotAnt(ant) {
+  currAnt = ant.drawing;
+  /*
+   *   ANT.DRAWING[] FORMAT:
+   *   Each index in ant.drawing[] is a single stroke
+   *   a stroke is a single line from pen down to pen up
+   *   NOTE: 'stroke' is called 'path'
+   *   because stroke() is a p5 function
+   *
+   *   Each path in ant.drawing[][] has two indexes:
+   *   [0] the x values for each vertex of the line
+   *   [1] the y values for each vertex of the line
+   *
+   *   Thus, a single vertex of a specific path is:
+   *   {
+   *     x: ant.drawing[path][0][i],
+   *     y: ant.drawing[path][1][i]
+   *   }
+   *
+   */
 }
